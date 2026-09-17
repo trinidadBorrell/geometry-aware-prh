8d7e649dac8cd101e5ad0b30670e2f008362871c
emilyzfliu <emily.zf.liu@gmail.com>
2026-09-16 23:11:53 -0700

    fix arg name bug

--- scripts/prh_readme_replicate.sh
+++ scripts/prh_readme_replicate.sh
@@ -4,11 +4,11 @@ pip install -r requirements.txt
 
 
 # extract all language model features and pool them along each block
-python extract_features.py --dataset minhuh/prh --subset wit_1024 --modelset val --modality language --pool avg --output-dir /workspace/results/emily/prh_features
+python extract_features.py --dataset minhuh/prh --subset wit_1024 --modelset val --modality language --pool avg --output_dir /workspace/results/emily/prh_features
 
 # Extract last layer features of all vision models
-python extract_features.py --dataset minhuh/prh --subset wit_1024 --modelset val --modality vision --pool cls --output-dir /workspace/results/emily/prh_features
+python extract_features.py --dataset minhuh/prh --subset wit_1024 --modelset val --modality vision --pool cls --output_dir /workspace/results/emily/prh_features
 
 
 python measure_alignment.py --dataset minhuh/prh --subset wit_1024 --modelset val \
-        --modality_x language --pool_x avg --modality_y vision --pool_y cls --input-dir /workspace/results/emily/prh_features --output-dir /workspace/results/emily/prh_alignment
+        --modality_x language --pool_x avg --modality_y vision --pool_y cls --input_dir /workspace/results/emily/prh_features --output_dir /workspace/results/emily/prh_alignment
