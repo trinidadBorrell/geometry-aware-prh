f96741662b81326256d93b7d1f688f9f6f1ab6ac
emilyzfliu <emily.zf.liu@gmail.com>
2026-09-19 22:50:54 -0700

    update for 0919

--- scripts/prh_readme_replicate.sh
+++ scripts/prh_readme_replicate.sh
@@ -7,8 +7,8 @@ pip install -r requirements.txt
 # python extract_features.py --dataset minhuh/prh --subset wit_1024 --modelset val --modality language --pool avg --output_dir /workspace/results/emily/prh_features
 
 # Extract last layer features of all vision models
-python extract_features.py --dataset minhuh/prh --subset wit_1024 --modelset val --modality vision --pool cls --output_dir /workspace/results/emily/prh_features
+# python extract_features.py --dataset minhuh/prh --subset wit_1024 --modelset val --modality vision --pool cls --output_dir /workspace/results/emily/prh_features
 
 
-# python measure_alignment.py --dataset minhuh/prh --subset wit_1024 --modelset val \
-#         --modality_x language --pool_x avg --modality_y vision --pool_y cls --input_dir /workspace/results/emily/prh_features --output_dir /workspace/results/emily/prh_alignment
+python measure_alignment.py --dataset minhuh/prh --subset wit_1024 --modelset val \
+        --modality_x language --pool_x avg --modality_y vision --pool_y cls --input_dir /workspace/hf/ --output_dir /workspace/results/emily/prh_alignment
