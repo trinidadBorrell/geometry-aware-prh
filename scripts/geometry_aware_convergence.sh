195e36d45724f350f8cdbbfc0857059623dd7b2a
emilyzfliu <emily.zf.liu@gmail.com>
2026-09-24 23:38:21 -0700

    fix script

--- scripts/geometry_aware_convergence.sh
+++ scripts/geometry_aware_convergence.sh
@@ -19,7 +19,7 @@ cd geometry_aware_convergence
 
 DISTS=(0.95 0.9 0.8 0.7 0.6)
 
-for k in "${KVALS[@]}"; do
+for dist in "${DISTS[@]}"; do
     python measure_alignment.py --modality_x all --modality_y all --metric cknda --dist "$dist"
     python measure_alignment.py --modality_x all --modality_y all --metric cknda --null-calibrate --topk "$dist"
 done
